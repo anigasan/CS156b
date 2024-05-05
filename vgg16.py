e886aedef8e5ee18d26b1e8e25f3fb703a6d53a8
anigasan <agangava@caltech.edu>
2024-05-04 20:23:56 -0700

    Added preliminary model code

--- vgg16.py
+++ vgg16.py
@@ -3,12 +3,30 @@ import torchvision
 from torchvision.models import resnet50
 
 
+num_epochs = 50
 
-lung_model = resnet50(pretrained = True)
+model = resnet50(pretrained = True)
+
+for param in model.parameters():
+    param.requires_grad = False
+
+model.fc = nn.Sequential(nn.Linear(2048, 256),
+                         nn.ReLU(), 
+                         nn.Linear(256, 9)) #We'll add the necessary layers in here, 256 is used as a placeholder
+    
 criterion = torch.nn.MSELoss()
-optimizer = torch.nn.Adam(lung_model.parameters(), lr=0.001)
+optimizer = torch.nn.Adam(model.parameters(), lr=0.001)
 
 
-#for epoch in range(100):
+#Training code
+model.train()
+for epoch in range(num_epochs):
+    for input_val, label in placeholder: #Will fill this in later
+        output = model(input_val)
+        loss = criterion(output, label)
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
     
     
